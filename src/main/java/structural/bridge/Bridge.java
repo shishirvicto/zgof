aefbd356e287464b7bb3ad2ed7e4400b544bf50f
shishir <shishirvicto@gmail.com>
2014-07-20 13:05:23 +0530

    added comments

--- src/main/java/structural/bridge/Bridge.java
+++ src/main/java/structural/bridge/Bridge.java
@@ -1,5 +1,17 @@
 package structural.bridge;
 
+/**
+ * Sometimes an abstraction should have different implementations; consider an
+ * object that handles persistence of objects over different platforms using
+ * either relational databases or file system structures (files and folders). A
+ * simple implementation might choose to extend the object itself to implement
+ * the functionality for both file system and RDBMS. However this implementation
+ * would create a problem; Inheritance binds an implementation to the
+ * abstraction and thus it would be difficult to modify, extend, and reuse
+ * abstraction and implementation independently.
+ * 
+ *
+ */
 public class Bridge {
 	public static void main(String[] args) {
 		Computer dell = new Dell(new Producer(), new Assembler());
@@ -8,5 +20,3 @@ public class Bridge {
 		apple.manufacture();
 	}
 }
-
- 
